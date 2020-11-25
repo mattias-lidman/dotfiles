84d4858d0de2345cf5aa75439988f19ff0a5cc8e
Mattias Lidman <mattias.lidman@gmail.com>
2020-11-24 22:48:35 -0600

    Add ripgrep binary

--- install.sh
+++ install.sh
@@ -30,6 +30,13 @@ for file in $DOTFILES; do
     fi
 done
 
+echo "Linking bin"
+if [ "$(uname)" == "Darwin" ]; then
+    ln -sf $PWD/macos_bin ~/bin
+else
+    ln -sfT $PWD/bin ~/bin
+fi
+
 # Run Vundle installs:
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
